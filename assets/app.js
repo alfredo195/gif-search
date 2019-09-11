d8500030c239b968e726a3af539ffaf965db6ce6
BlackAce <alfredo.gon195@gmail.com>
2019-09-11 02:17:44 -0700

    added if statment for taking the still to animate

--- assets/app.js
+++ assets/app.js
@@ -38,6 +38,13 @@ $(document).ready(function() {
             gifDiv.append(animalGif);
             $("#animal-view").prepend(gifDiv);
         }
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
 
 
       })
