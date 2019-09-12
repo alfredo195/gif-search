73f3b2a64ffd1b5fd02ea5e93d4beb3ad3af5164
BlackAce <alfredo.gon195@gmail.com>
2019-09-12 01:18:00 -0700

    cleaned up code and now it works

--- assets/app.js
+++ assets/app.js
@@ -1,109 +1,99 @@
 $(document).ready(function() {
+  var animalList = [
+    "hippopotamus",
+    "koala",
+    "pig",
+    "puma",
+    "snake",
+    "dog",
+    "cat",
+    "alpaca",
+    "frog",
+    "cow",
+    "donkey",
+    "armadillo",
+    "seal",
+    "gorilla"
+  ];
 
-    var animalList = [
-      "hippopotamus",
-      "koala",
-      "pig",
-      "puma",
-      "snake",
-      "dog",
-      "cat",
-      "alpaca",
-      "frog",
-      "cow",
-      "donkey",
-      "armadillo",
-      "seal",
-      "gorilla",
-    ];
-
-    function displayAnimals(){
-        var animalList = $(this).attr("")
-
-        var queryURL =
-      "https://api.giphy.com/v1/gifs/search?q=" +
-      animal +
-      "&api_key=qhZP9JnDanZ5O7LQzXiFYgX18hKKgdFX&limit=10";
-
-      $.ajax({
-          url: queryURL,
-          method: "GET"  
-      }).then(function(response){
-        var results = response.data
-
-        for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div class = 'animal'>");
-            var animalGif = $("<img>")
-            animalGif.attr("src",results[i].images.fixed_height.url);
-            gifDiv.append(animalGif);
-            $("#animal-view").prepend(gifDiv);
-        }
-        if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-          } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-          }
-
-
-      })
-        
-    }
-
-  $("#find-animal").on("click", function(event) {
-    event.preventDefault();
-
-
-    var animal = $("#animal-input").valueOf();
-
+  function displayImg() {
+    $("#animal-view").empty();
+    var input = $(this).attr("data-name");
+    var limit = 10;
     var queryURL =
       "https://api.giphy.com/v1/gifs/search?q=" +
-      animal +
+      input +
+      "&limit=" +
+      limit +
       "&api_key=qhZP9JnDanZ5O7LQzXiFYgX18hKKgdFX&limit=10";
-  });
 
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      for (var i = 0; i < limit; i++) {
+        var displayDiv = $("<div>");
+        displayDiv.addClass("holder");
+
+        var image = $("<img>");
+        image.attr("src", response.data[i].images.original_still.url);
+        image.attr("data-still", response.data[i].images.original_still.url);
+        image.attr("data-animate", response.data[i].images.original.url);
+        image.attr("data-state", "still");
+        image.attr("class", "gif");
+        displayDiv.append(image);
+
+        var rating = response.data[i].rating;
+        console.log(response);
+        var pRating = $("<p>").text("Rating: " + rating);
+        displayDiv.append(pRating);
+
+        $("#animal-view").append(displayDiv);
+      }
+    });
+  }
 
   function renderButtons() {
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
-    $("#buttons-view").empty();
+    $("#display-buttons").empty();
 
-    // Looping through the array of movies
     for (var i = 0; i < animalList.length; i++) {
-      // Then dynamicaly generating buttons for each movie in the array
-      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-      var a = $("<button>");
-      // Adding a class of movie-btn to our button
-      a.addClass("gif-btn");
-      // Adding a data-attribute
-      a.attr("data-name", animalList[i]);
-      // Providing the initial button text
-      a.text(animalList[i]);
-      // Adding the button to the buttons-view div
-      $("#buttons-view").append(a);
+      var newButton = $("<button>");
+      newButton.attr("class", "btn btn-default");
+      newButton.attr("id", "input");
+      newButton.attr("data-name", animalList[i]);
+      newButton.text(animalList[i]);
+      $("#display-buttons").append(newButton);
     }
   }
 
-  // This function handles events where a movie button is clicked
-  $("#find-animal").on("click", function(event) {
-    event.preventDefault();
-    // This line grabs the input from the textbox
-    var movie = $("#animal-input")
+  function imageChangeState() {
+    var state = $(this).attr("data-state");
+    var animateImage = $(this).attr("data-animate");
+    var stillImage = $(this).attr("data-still");
+
+    if (state == "still") {
+      $(this).attr("src", animateImage);
+      $(this).attr("data-state", "animate");
+    } else if (state == "animate") {
+      $(this).attr("src", stillImage);
+      $(this).attr("data-state", "still");
+    }
+  }
+
+  $("#submitPress").on("click", function() {
+    var input = $("#user-input")
       .val()
       .trim();
-    $("#animal-input").val("");
-    // Adding movie from the textbox to our array
-    animalList.push(animalList.trim());
+    form.reset();
+    animalList.push(input);
 
-    // Calling renderButtons which handles the processing of our movie array
     renderButtons();
+
+    return false;
   });
 
-  // Adding a click event listener to all elements with a class of "movie-btn"
-  $(document).on("click", ".gif-btn", displayAnimals);
-
-  
   renderButtons();
 
+  $(document).on("click", "#input", displayImg);
+  $(document).on("click", ".gif", imageChangeState);
 });
